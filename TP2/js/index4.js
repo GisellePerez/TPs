8ff41852c6e624af37cb6f4f643eb9c9c523e110
GisellePerez <giselleperez91@gmail.com>
2018-05-18 02:31:57 -0300

    fixed match misma ficha

--- TP2/js/index4.js
+++ TP2/js/index4.js
@@ -21,6 +21,8 @@ var intentos=24;
 var aciertos=0;
 var puntosIntentos;
 var cont=0;
+var selected=[];
+
 
 var pair = [];
 
@@ -29,7 +31,6 @@ function playerName(){
     let name = prompt('Please enter your name', '');
 
     if (name != null) {
-        //$('#person').html = 'Welcome'  + person + "! Let's play!";
         let welcomePerson = `<p id="person">Welcome ${name}!</p>`;
         div.append(welcomePerson);        
     }else{
@@ -41,8 +42,11 @@ function playerName(){
 function displayScore(){
     let div = $('#infoJuego');
     let p = `<p>Movimientos disponibles: ${puntosIntentos}</p>`;
-    //no se actualiza el valor :/
+    //no se actualiza el valor :(
     //div.append(p);
+    /*function chancesContador(){
+        $('#chancesIntentos').text('Intentos: '+ intentos);
+    }*/
 }
 
 /*
@@ -54,7 +58,7 @@ function shuffle(){
     var random=0;
     var x=0;
     for(i=0;i<fichas.length;i++){
-        random = Math.round(Math.random()+1); //tira un numero random
+        random = Math.round(Math.random()+1); //da un numero random
         x = fichas[i]; //indice, lo necesito para que vaya iterando TODO el array
         fichas[i]=fichas[random]; //asigna el nro random al index de las fichas
         fichas[random]=x; // x = al nro random
@@ -72,7 +76,7 @@ function asignTiles(){
         
     $('.ficha').each(function(index,elem){
 
-        $(this).attr('data-par',fichas[index].par); //le asigno el data- = al index. Debería cambiar segun el shuffle
+        $(this).attr('data-par',fichas[index].par); //le asigno el data- = al index. Cambia segun el shuffle
        
         let img ='<img class="hidden" src="'+fichas[index].img+'">';
         var ficha = $("#ficha"+(index+1));
@@ -115,39 +119,62 @@ function gameOver(){
 */
 
 $('.ficha').on('click', function(){
-    $(this).children().first().toggleClass('visible hidden'); //me trae la imagen y no el div y cambia la clase
-    console.log($(this).children().first()); //consolea la img (hija del div)
-    
-    if (cont < 2){
+
+    if(cont < 2){
         cont++;
-        if(firstClick == null){
-            firstClick= $(this).children().first();
-            console.log('first: '+firstClick);
-        }else{
-            var lastClick= $(this).children().first();
-            console.log('last: '+lastClick);             
-                                
+        
+            $(this).children().first().toggleClass('visible hidden'); //me trae la imagen y no el div y cambia la clase
+            console.log($(this).children().first()); //consolea la img (hija del div)
+        
+            if(firstClick == null){
+                firstClick= $(this).children().first();
+                console.log('first: '+firstClick);
+                selected.push(firstClick);
+                firstClick.parent().addClass('noPointer'); // para que no permita un segundo click sobre el elemento
+
+            }else{
+                var lastClick= $(this).children().first();
+                console.log('last: '+lastClick);
+                selected.push(lastClick);
+                lastClick.parent().addClass('noPointer'); // para que no permita un segundo click sobre el elemento
+                            
+                console.log(firstClick.parent()); 
                 
-                if(firstClick.attr('src') == lastClick.attr('src')){
-                    console.log("MATCH! :D");
-                    aciertos++;
-                    console.log('aciertos: '+aciertos);
+                console.log('selected: '+selected);  
+                
+                
+
+                    if(firstClick.attr('src') == lastClick.attr('src')//&& selected[0].parent().attr('id') != selected[1].parent().attr('id')
+                        && firstClick.parent().attr('id') != lastClick.parent().attr('id')){
+
+                        console.log("MATCH! :D");
+                        selected.splice(0,1);
+                        selected=[];
+                        aciertos++;
+                        console.log('aciertos: '+aciertos); 
                     
-                    firstClick= null;
-                    lastClick= null;       
-                
-                }else{
-                    setTimeout(flip, 1200, firstClick, lastClick);
-                    intentos--;
-                    console.log('intentos: '+intentos);
-                }
+                        firstClick= null;
+                        lastClick= null; 
+                            
+                    
+                    }else{
+                        firstClick.parent().addClass('background');
+                        firstClick.parent().removeClass('noPointer'); // para poder clickear de nuevo
+                        lastClick.parent().removeClass('noPointer'); // para poder clickear de nuevo
+
+                        selected=[];
+                        setTimeout(flip, 1200, firstClick, lastClick);
+                        intentos--;
+                        console.log('intentos: '+intentos);
+                       
+                    }
                 firstClick= null;
                 lastClick= null;
-            
-        }
-        victory();
-        gameOver();
-    }    
+            }
+        
+    }   
+    victory();
+    gameOver();
 });
 
 $('#btnRestart').on('click', function(){
