b45e4861d5100564386db3b19f4a4e6f68cd39d9
GisellePerez <giselleperez91@gmail.com>
2018-06-06 04:35:43 -0300

    version final

--- TP3/js/index.js
+++ TP3/js/index.js
@@ -1,7 +1,6 @@
 /*
 *   VARIABLES
 */
-
 var fichas = [
     {'par':'1', 'img':'img/A.jpg'},{'par':'2', 'img':'img/B.jpg'},{'par':'3', 'img':'img/C.jpg'},
     {'par':'4', 'img':'img/D.jpg'},{'par':'5', 'img':'img/E.jpg'},{'par':'6', 'img':'img/F.jpg'},
@@ -9,26 +8,36 @@ var fichas = [
     {'par':'4', 'img':'img/D.jpg'},{'par':'5', 'img':'img/E.jpg'},{'par':'6', 'img':'img/F.jpg'}];
 
 var arrayLevels =['Fácil','Intermedio','Experto'];
+
 var firstClick= null;
-var intentos;
+var intentos=0;
 var aciertos=0;
+var puntosIntentos;
 var cont=0;
 var selected=[];
-var puntaje=0;
+var playerName;
 var puntajeFinal=0;
-var playerName; /*usada en función getPlayerName*/
-var playerInfo = {};
-var players = [];
-var datosGuardados = localStorage.getItem('players');
-var lost=false; var won=false;
+var puntaje=0;
+var lost=false; 
+var won =false;
 
+/*
+*   Funcion que carga las options para seleccionar el nivel
+*/
 
+function loadLvlOptions() {
+    for(var i=0 ; i<arrayLevels.length ; i++) {
+        var option = `<option class="levelOp" value="${arrayLevels[i]}">${arrayLevels[i]}</option>`; 
+        $('#lvlSelect').append(option);
+    }
+}
 
 /*
 *   Funcion para ingresar nombre
 */
 
 function getPlayerName(){
+
     let div = $('#nameDiv');
     playerName = $('#playerNameInput').val();
     console.log('name '+playerName);
@@ -37,91 +46,16 @@ function getPlayerName(){
         let welcomePerson = `<p class="person">Bienvenida/o <span id = "playerName">${playerName}</span>!</p>`;
         div.append(welcomePerson);
     }else{
-        let welcome = `<p class="person">Bienvenida/o!</p>`;
+        playerName = 'Anon';
+        let welcome = `<p class="person">Bienvenida/o! <span id = "playerName">${playerName}</span></p>`;
         div.append(welcome);
     }
-
 }
-
-function loadLvlOptions() {
-    for(var i=0 ; i<arrayLevels.length ; i++){
-        var option = `<option value="${arrayLevels[i]}"></option>`; 
-        option.text(arrayLevels[i]);
-        $('#docType').append(option);
-    }
-}
-/*
-*  Spinner 
-*/
-/*
-$.widget("ui.textSpinner", $.ui.spinner, {
-    options: {
-        wrap: true
-    },
-    _parse: function (value) {                                                                                                                                              
-        if ((value === '') || isNaN(value)) {
-            value = this.options.values.indexOf(value);
-            if (value === -1) {
-                value = 0;
-            }
-        }
-        if (value < 0) {
-            value = this.options.wrap ? (this.options.values.length -1) : 0;
-        } else if (value >= this.options.values.length) {
-            value = this.options.wrap ? 0 : (this.options.values.length - 1);
-        }
-        return value;
-    },
-    _format: function (value) {
-        return this.options.values[value];
-    },
-    _adjustValue: function (value) {
-        if (value < 0) {
-            value = this.options.wrap ? (this.options.values.length - 1) : 0;
-        } else if (value >= this.options.values.length) {
-            value = this.options.wrap ? 0 : (this.options.values.length - 1);
-        }
-        return value;
-    }
-}); 
-
-$(function() {
-    $("#spinner").textSpinner({
-        values: arrayLevels,
-        spin: function( event, ui ) {
-           $( "#spinner-val" ).text(ui.value);
-        }
-    });
-});
-*/
-
-/**
- * Listenner del boton para elegir el nivel
- */
-
-$('#jugar').on('click', function() {
-
-    $('#welcomeDiv').hide();
-
-    console.log( $('#spinner-val').text() );
-    let level = ( $('#spinner-val').text() );
-
-    switch(level) {        
-        case '0'/*fácil*/: intentos = 18; break;        
-        case '1'/*Intermedio*/: intentos = 12; break;        
-        case '2'/*Experto*/: intentos = 8; break;        
-        default: 'No level';
-    }
-    //console.log(intentos); //ahora pasarlo al append
-    getPlayerName();
-    displayMoves(intentos);
-
-    return intentos;    
-});
 
 /*
 *   Funcion que carga los intentos
 *   @ params | intentos
+*   return intentos
 */
 
 function displayMoves(intentos){
@@ -130,9 +64,11 @@ function displayMoves(intentos){
     div.append(p);
 }
 
+
 /*
-*   Funcion que carga el puntaje
-*   @ params | puntaje
+*   Funcion que muestra los puntos
+*   @ | puntaje
+*   return puntaje
 */
 
 function displayScore(puntaje) {
@@ -150,10 +86,10 @@ function shuffle(){
     var random=0;
     var x=0;
     for(i=0;i<fichas.length;i++){
-        random = Math.round(Math.random()+1); //da un numero random
-        x = fichas[i]; //indice, lo necesito para que vaya iterando TODO el array
-        fichas[i]=fichas[random]; //asigna el nro random al index de las fichas
-        fichas[random]=x; // x = al nro random
+        random = Math.round(Math.random()+1);
+        x = fichas[i]; 
+        fichas[i]=fichas[random]; 
+        fichas[random]=x; 
     }
     asignTiles(); // la llamo acá para que me asigne los data- despues del shuffle
     console.log(fichas);
@@ -189,13 +125,11 @@ function flip(firstClick, lastClick){
 */
 
 function victory(){
+    let victoryP = `<p class="rankingTitle"> Victory! </p>`;
     if(aciertos==6){
-        setTimeout(function(){ alert('VICTORY!'); /*displayRanking();*/}, 400); 
         won=true; //bandera
-        console.log('won: '+won);       
+        setTimeout(function(){ $('#rankingTitleDiv').append(victoryP); displayRanking(); }, 500);         
     }
-    //savePlayerData();
-    console.log('llamo a victory');  
 }
 
 /*
@@ -203,167 +137,152 @@ function victory(){
 */
 
 function gameOver(){
+    let gameOverP = `<p class="rankingTitle"> Game over </p>`;
     if(intentos==0 && lost == false /*si no pongo el booleano funciona en todos los clicks*/){
-        setTimeout(function(){ alert('GAME OVER!');  /*displayRanking();*/ }, 400); 
-        lost=true;
-        console.log('lost: '+lost);              
-    }
-    //savePlayerData();
-    console.log('llamo a game over');
-}
-
-function savePlayerData(name,score){
-    console.log('llamo a savePlayerData');
-    console.log('playerName :'+playerName);
-    console.log('puntajeFinal :'+puntajeFinal);
-
-    playerInfo = {'name': playerName, 'score': puntajeFinal};
-    console.log('playerInfo: '+playerInfo);
-
-    if(datosGuardados==null){
-        players = [];
-    }else{
-        players = JSON.parse(datosGuardados).players; 
-    }
-    console.log('datos guardados: '+datosGuardados);
-    players.push(playerInfo);
-
-    let jsonPlayerInfo = {'players':players,'total':players.length}
-    console.log('jsonPlayerInfo: '+jsonPlayerInfo);
-
-    let data = JSON.stringify(jsonPlayerInfo);
-    
-    localStorage.setItem('players',data);
-
-    console.log('players:'+players);
-
-}
-
-function displayRanking() {
-
-     if(lost==true || won == true ) {   
-       
-        $( "#dialogRanking" ).removeClass('displayRanking');
-        $( "#dialogRanking" ).addClass('displayRanking');
-    
-        for(var i=0; i<players.length; i++){
-            var playerNameList = players[i].name;
-            var playerScoreList = players[i].score;
-        }  
-
-        $( "#dialogRanking" ).append(playerNameList);
-        $( "#dialogRanking" ).append(playerScoreList);
-
+        lost=true; //bandera  
+        setTimeout(function(){ $('#rankingTitleDiv').append(gameOverP); displayRanking(); }, 500);              
     }
 }
+
 /*
-function checkWin() {
-    if(won == true || lost == true ){
-        console.log(playerName);
-        console.log(puntajeFinal);
-        savePlayerData(playerName, puntajeFinal);
-        //displayRanking();
-        
-    }
-} */    
+*   Funcion que guarda los datos del jugador actual en el localStorage y lo pushea en un array players.
+*   Luego de ordenar el array según el score de cada jugador, se hace el stringify y el setItem
+*/
+  
+function savePlayerData() {
+    var jsonPlayerInfo;
+    var players;  
+    let datosGuardados = localStorage.getItem("players");
 
+    if (datosGuardados == null) {
+        players = [];
+    } else {
+        players = JSON.parse(datosGuardados).players;
+    }
+  
+    let data = { name: playerName, score: puntajeFinal }; //desordenado
+    players.push(data);  
+    sortedPlayers = players.sort((a, b) => b.score - a.score); //ordena de mayor a menor puntaje
+
+    jsonPlayerInfo = { players: sortedPlayers, total: sortedPlayers.length }; //ordenado :D
+    let jsonPlayer = JSON.stringify(jsonPlayerInfo);
+    localStorage.setItem("players", jsonPlayer);
+} 
+
+/*
+* Función que muestra la información alojada en el localStorage 
+*/
+
+function showPlayerData() {
+    let datosGuardadosParse = JSON.parse(localStorage.getItem("players"));
+    let rankingInfoDiv = $('#rankingInfoDiv');
+    //let ul = $("<ul></ul>").addClass('rankingUl');
+
+    for (var i = 0; i < datosGuardadosParse.players.length; i++) {
+        let div = `<div><p class="rankingNames">${datosGuardadosParse.players[i].name}</p> 
+                    <p class="rankingScores">${datosGuardadosParse.players[i].score}</p></div>`        
+        rankingInfoDiv.append(div);
+        //cambiar el display del div pop up y organizar la info
+    }
+}
+
+function displayRanking(){
+    $('#rankingDiv').removeClass('noDisplayRanking'); 
+    $('#rankingDiv').addClass('displayRanking'); 
+    $('.ficha').addClass('noPointer');
+}
+    
 /*
 *   Listeners
 */
 
+$('#jugar').on('click', function(){
+    let level = ($("#lvlSelect option:selected").val());
+    if(level != -1){
+        $('#welcomeDiv').hide();
+        switch(level) {
+            case 'Fácil': intentos = 18; break;        
+            case 'Intermedio': intentos = 12; break;        
+            case 'Experto': intentos = 8; break;        
+            default: 'No level';        
+        }
+        getPlayerName();
+        displayMoves(intentos);
+        displayScore(puntaje); 
+
+    }else{
+        $('.oblig').text('DEBES ELEGIR UN NIVEL');
+    }     
+});
+
 $('.ficha').on('click', function(){
     if (cont < 2){  //para evitar más de dos click por turno   
         $(this).children().first().toggleClass('visible hidden'); //me trae la imagen y no el div y cambia la clase
-        console.log($(this).children().first()); //consolea la img (hija del div)
                
         if(firstClick == null){
             firstClick= $(this).children().first();
             console.log('first: '+firstClick);
             firstClick.parent().addClass('noPointer'); // para que no permita un segundo click sobre el elemento
+            $(firstClick).effect( "pulsate", {times:2}, "slow"); /* EFECTO */   
                    
         }else{
             var lastClick= $(this).children().first();
             console.log('last: '+lastClick);
-            lastClick.parent().addClass('noPointer'); // para que no permita un segundo click sobre el elemento      
-            console.log('selected: '+selected);         
-                 
-                if(firstClick.attr('src') == lastClick.attr('src')//&& selected[0].parent().attr('id') != selected[1].parent().attr('id')
+            lastClick.parent().addClass('noPointer'); // para que no permita un segundo click sobre el elemento
+            $(lastClick).effect( "pulsate", {times:2}, "slow"); /* EFECTO */        
+                if(firstClick.attr('src') == lastClick.attr('src')
                     && firstClick.parent().attr('id') != lastClick.parent().attr('id')){
 
-                    console.log("MATCH! :D");                            
-                    console.log('sel: '+selected.length);
-                            
                     aciertos++;
-                    console.log('aciertos: '+aciertos); 
+                    puntaje = puntaje += 100;                      
                     firstClick= null;
-                    lastClick= null;                  
-                    puntaje = puntaje += 100;
-                    console.log('algo match');    
-                    
+                    lastClick= null;   
+
                 }else{
 
                     firstClick.parent().addClass('background');
                     firstClick.parent().removeClass('noPointer'); // para poder clickear de nuevo
                     lastClick.parent().removeClass('noPointer'); // para poder clickear de nuevo
-
                     setTimeout(flip, 800, firstClick, lastClick);
                     intentos--;
-                    console.log('intentos: '+intentos);                    
                     if (puntaje > 0){ //para que no tenga valores negativos
                         console.log('algo no match');
                         puntaje = puntaje -= 20;
                     }
-                   
-                }
-    
-            firstClick= null;
-            lastClick= null;
-            selected=[]; 
-                              
-        }
-        cont++; //Acá cuenta bien
-        console.log('contadorIf: '+cont);
-        console.log('puntaje final :'+puntaje); //acá toma el puntaje final
+                }   
 
-        
-        //displayScore(puntaje);
-        //displayMoves(intentos);
-        //savePlayerData();
-        //victory();
-        //gameOver(); 
-        
-              
+            firstClick= null;
+            lastClick= null;                   
+        }
+        cont++; 
+        console.log('contadorIf: '+cont);                         
     }
     if(cont == 2){
-        setTimeout(function(){ cont=0; console.log('contadorElse: '+cont);}, 800); 
-        //con tiempo porque sino vuelve a 0 antes de que las teclas se den vuelta 
+        setTimeout(function(){ cont=0; console.log('contadorElse: '+cont);}, 800); //con tiempo porque sino vuelve a 0 antes de que las teclas se den vuelta 
     }
+    victory();
+    gameOver();
+    displayMoves(intentos);
+    displayScore(puntaje);
 
     if(intentos == 0 || aciertos == 6){
         puntajeFinal = puntaje;
-        console.log('puntaje final : '+puntajeFinal);
+        console.log('puntaje final : '+ puntajeFinal);
+        console.log('nombre: ' + playerName);
+        savePlayerData();  
+        showPlayerData();        
     }
-    displayScore(puntaje);
-    displayMoves(intentos);
-    victory();
-    gameOver();
-    checkWin();
 });
 
+$('#restart').on('click', function(){
+        window.location.reload();
+});
+  
 /*
 *   Llamada a funciones
 */
-
 shuffle();
-//getPlayerName();
-displayScore(puntaje); //para que aparezca (en cero) antes de comenzar los intentos.
-//checkWin();
-//victory();
-//gameOver();
-
+loadLvlOptions();
 //localStorage.clear();
-
-
-
 
 
